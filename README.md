b4f695aa1224941ac5342693cccb16bc5177138a
vbrh-immalle <hans.vanbroeckhoven@immalle.be>
2017-04-27 12:54:51 +0000

    Make a new artisan command with a controller

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # CLI commands
 
 - add commands to *the easy way with callbacks* to `routes/console.php`
+- add commands *the hard way*
+    - `php artisan make:command HelloWorld`
+    - edit the `app/Console/Commands/HelloWorld.php` file
+    - register the `HelloWorld`-class in the commands in `app/Console/Kernel.php`
---
+++ app/Console/Commands/HelloWorld.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class HelloWorld extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'hello:world';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Say hello to the world.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->comment("Hello!!!");
+    }
+}
